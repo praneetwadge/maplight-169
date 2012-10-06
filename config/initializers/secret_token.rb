52cebdae346c0fa35f1ca0d59519bf26094071f3
Varun Pai <varunpai12@gmail.com>
2012-10-05 21:00:50 -0700

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maplight::Application.config.secret_token = 'aecdb97cfc69c1c6e83fe86e560c3b63018a37dfe53e06cbac5b68a83985706e68ac6aa01ca029384b812bd312efbd4286fb0c9f2f6756e553f1769b15ac5f7e'
