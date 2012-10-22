b083281826df71459e43569fdc81d1b9e844c9bb
Ayushi Samaddar <ayushi_sen@yahoo.com>
2012-10-21 19:56:21 -0700

    added simplecov

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.1.0'
 
 # for Heroku deployment - as described in Ap. A of ELLS book
 group :development, :test do
+	gem 'simplecov', :require => false, :group => :test
   gem 'sqlite3'
   gem 'ruby-debug19', :require => 'ruby-debug'
   gem 'database_cleaner'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-contributions = [{:date => Date.new(2010, 11, 4), :contributor => "Jeanne Buell", :amount => 10000, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
-								{:date => Date.new(2010, 10, 31), :contributor => "Jeanne Buell", :amount => 2000, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
-								{:date => Date.new(2010, 4, 4), :contributor => "J A Buell", :amount => 100, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
-								{:date => Date.new(2009, 2, 28), :contributor => "Jeanne Buell", :amount => 200, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
-								{:date => Date.new(2009, 1, 15), :contributor => "Jeanne Buell", :amount => 10000, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
-								{:date => Date.new(2012, 10, 12), :contributor => "Ayushi Samaddar", :amount => 100, :city => "Berkeley", :state => "CA", :recipient => "Ronald Reagan"},
-								{:date => Date.new(2012, 10, 12), :contributor => "Praneet Wagde", :amount => 100, :city => "Berkeley", :state => "CA", :recipient => "John Snow"},
-								{:date => Date.new(2012, 10, 13), :contributor => "Praneet Wagde", :amount => 200, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},
-								{:date => Date.new(2012, 10, 14), :contributor => "Praneet Wagde", :amount => 200, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},
-								{:date => Date.new(2012, 10, 12), :contributor => "Praneet W", :amount => 300, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},
-								{:date => Date.new(2012, 10, 12), :contributor => "P Wagde", :amount => 150, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},]
+contributions = [{:date => Date.new(2010, 11, 4), :donor => "Jeanne Buell", :amount => 10000, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
+								{:date => Date.new(2010, 10, 31), :donor => "Jeanne Buell", :amount => 2000, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
+								{:date => Date.new(2010, 4, 4), :donor => "J A Buell", :amount => 100, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
+								{:date => Date.new(2009, 2, 28), :donor => "Jeanne Buell", :amount => 200, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
+								{:date => Date.new(2009, 1, 15), :donor => "Jeanne Buell", :amount => 10000, :city => "San Francisco", :state => "CA", :recipient => "Nancy Pelosi"},
+								{:date => Date.new(2012, 10, 12), :donor => "Ayushi Samaddar", :amount => 100, :city => "Berkeley", :state => "CA", :recipient => "Ronald Reagan"},
+								{:date => Date.new(2012, 10, 12), :donor => "Praneet Wagde", :amount => 100, :city => "Berkeley", :state => "CA", :recipient => "John Snow"},
+								{:date => Date.new(2012, 10, 13), :donor => "Praneet Wagde", :amount => 200, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},
+								{:date => Date.new(2012, 10, 14), :donor => "Praneet Wagde", :amount => 200, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},
+								{:date => Date.new(2012, 10, 12), :donor => "Praneet W", :amount => 300, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},
+								{:date => Date.new(2012, 10, 12), :donor => "P Wagde", :amount => 150, :city => "Berkeley", :state => "CA", :recipient => "Jane Smith"},]
 
 contributions.each do |contribution|
 	Contribution.create!(contribution)
